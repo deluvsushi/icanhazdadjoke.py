19d2f5e908319b1a633649f429207e50903200f9
ecclesias <77536370+zeviel@users.noreply.github.com>
2022-11-19 20:33:50 +0800

    Update icanhazdadjoke.py

--- src/icanhazdadjoke.py
+++ src/icanhazdadjoke.py
@@ -1,36 +1,36 @@
 from requests import get
 
-
 class Icanhazdadjoke:
-	def __init__(self):
+	def __init__(self) -> None:
 		self.api = "https://icanhazdadjoke.com"
 		self.headers = {
 			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36",
 			"accept": "application/json"
 		}
 	
-	def get_random_dad_joke(self):
+	def get_random_dad_joke(self) -> dict:
 		return get(
-			f"{self.api}/", headers=self.headers).json()
+			f"{self.api}/",
+			headers=self.headers).json()
 	
-	def get_random_dad_joke_as_slack(self):
+	def get_random_dad_joke_as_slack(self) -> dict:
 		return get(
 			f"{self.api}/slack",
 			headers=self.headers).json()
 	
-	def get_dad_joke(self, joke_id: str):
+	def get_dad_joke(self, joke_id: str) -> dict:
 		return get(
 			f"{self.api}/j/{joke_id}",
 			headers=self.headers).json()
 	
-	def get_dad_joke_as_image(self, joke_id: str):
+	def get_dad_joke_as_image(self, joke_id: str) -> str:
 		return f"{self.api}/j/{joke_id}.png"
 	
 	def search_dad_jokes(
 			self,
 			page: int = 1,
 			limit: int = 20, # max = 30
-			term: str = "list all jokes"):
+			term: str = "list all jokes") -> dict:
 		return get(
 			f"{self.api}/search?page={page}&limit={limit}&term={term}",
 			headers=self.headers).json()
